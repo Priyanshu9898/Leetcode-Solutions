cf0180ffc8ea1135a73d7cf313ecde962687ecb9
Priyanshu Malaviya <87270273+Priyanshu9898@users.noreply.github.com>
2022-08-03 16:25:00 +0530

    Time: 15 ms (43.12%), Space: 13.5 MB (98.17%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -8,14 +8,13 @@ public:
         int s = 0;
         int e = nums.size() - 1;
         
-        
         while(s <= e){
             int mid = (s +e)/2;
             
             // cout << mid << endl;
             if(nums[mid] == target){
                 first = mid;
-                e = mid - 1;
+                break;
             }
             
             else if(nums[mid] > target){
@@ -26,29 +25,28 @@ public:
             }
         }
         
-        ans.push_back(first);
-        int second = first;
-        s = 0;
-        e = nums.size() - 1;
+      
         
-        while(s <= e){
-            int mid = (s +e)/2;
-            
-            // cout << mid << endl;
-            if(nums[mid] == target){
-                second = mid;
-                s = mid + 1;
-            }
-            
-            else if(nums[mid] > target){
-                e = mid - 1;
-            }
-            else{
-                s = mid + 1;
-            }
+        if(first == -1){
+            return {-1, -1};
         }
-        ans.push_back(second);
         
+        else{
+            
+            
+            while(first > 0 && nums[first-1] == target){
+                first--;
+            }
+            
+            ans.push_back(first);
+            
+            while(first < nums.size()-1 && nums[first+1] == target){
+                first++;
+            }
+            
+            ans.push_back(first);
+            
+        }
         
         return ans;
     }
